11606077a7a8b925e6e842873eec89a98cdfe041
Armando Rodríguez <jarodriguezvera@gmail.com>
2022-03-18 11:42:19 -0600

    Agregar archivos de proyecto.

---
+++ LineaBlanca.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace EjemplosInterfaces
+{
+    abstract class LineaBlanca
+    {
+        #region Atributos
+        private string marca;
+        private string modelo;
+        #endregion
+
+        #region Propiedades
+        public string Marca { 
+            get => marca;
+            set
+            {
+                if( value == "")
+                {
+                    marca = "LG";
+                }
+                else
+                {
+                    marca = value;
+                }                
+            }
+        }
+        public string Modelo { get => modelo;
+            set
+            {
+                if( value == "")
+                {
+                    modelo = "2022";
+                }
+                else
+                {
+
+                    modelo = value;
+                }
+                
+            }
+        }
+        #endregion
+    }
+}
